ac3ccd93bf3a61ef3a37bb138df12ddcaa13d34d
higorrfa <higor.araujo@ccc.ufcg.edu.br>
2021-07-20 14:06:10 -0300

    feat: registro condicionais dos microfrontends a partir de json mockado

--- single-spa/src/virtus-easy-root-config.js
+++ single-spa/src/virtus-easy-root-config.js
@@ -9,35 +9,32 @@ import { registerApplication, start } from "single-spa";
 //   activeWhen: ["/"],
 // });
 
-registerApplication({
-  name: "@virtus-easy/react-counter",
-  app: () =>
-    System.import(
-      "@virtus-easy/react-counter"
-    ),
-  activeWhen: ["/"],
-});
+// registerApplication({
+//   name: "@virtus-easy/react-counter",
+//   app: () =>
+//     System.import(
+//       "@virtus-easy/react-counter"
+//     ),
+//   activeWhen: ["/"],
+// });
 
-registerApplication({
-  name: "@virtus-easy/react-todo-list",
-  app: () =>
-    System.import(
-      "@virtus-easy/react-todo-list"
-    ),
-  activeWhen: ["/"],
-});
+// registerApplication({
+//   name: "@virtus-easy/react-todo-list",
+//   app: () =>
+//     System.import(
+//       "@virtus-easy/react-todo-list"
+//     ),
+//   activeWhen: ["/"],
+// });
 
-registerApplication({
-  name: "@virtus-easy/react-mega",
-  app: () =>
-    System.import(
-      "@virtus-easy/react-mega"
-    ),
-  activeWhen: ["/"],
-});
-
-
-
+// registerApplication({
+//   name: "@virtus-easy/react-mega",
+//   app: () =>
+//     System.import(
+//       "@virtus-easy/react-mega"
+//     ),
+//   activeWhen: ["/"],
+// });
 
 // registerApplication({
 //   name: "@virtus-easy/navbar",
@@ -45,6 +42,27 @@ registerApplication({
 //   activeWhen: ["/"]
 // });
 
-start({
-  urlRerouteOnly: true,
-});
+// start({
+//   urlRerouteOnly: true,
+// });
+
+fetch('https://run.mocky.io/v3/b111500f-baf2-4d74-9882-7ef773a9b736')
+  .then(resp => resp.json())
+  .then(data => {
+    data.applications.forEach(app => {
+      if (app.selected) {
+        registerApplication({
+          name: app.name,
+          app: () => System.import(app.package),
+          activeWhen: app.exact
+            ? (location) => location.pathname === app.activeWhen
+            : [app.activeWhen]
+        });
+      }
+    })
+  })
+  .finally(() => {
+    start({
+      urlRerouteOnly: true,
+    })
+  })
